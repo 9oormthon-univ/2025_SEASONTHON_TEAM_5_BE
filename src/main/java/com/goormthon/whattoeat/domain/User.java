e6a09c7d566eb02784926c8fad1febcaa71f4837
박기찬 <apg0001naver@gmail.com>
2025-09-03 22:18:24 +0900

    [chore] gradle.yml에 RDS 설정 추가

---
+++ src/main/java/com/goormthon/whattoeat/domain/User.java
@@ -0,0 +1,15 @@
+package com.goormthon.whattoeat.domain;
+
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.AllArgsConstructor;
+//
+//import java.util.Date;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class User {
+}
