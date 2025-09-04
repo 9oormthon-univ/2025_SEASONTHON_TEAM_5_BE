25ada2177e25673805e165ea0dd7e30eac217db7
박기찬 <apg0001naver@gmail.com>
2025-09-04 22:44:43 +0900

    [feat] 재료 레포지토리 추가

---
+++ src/main/java/com/goormthon/whattoeat/repository/IngredientRepository.java
@@ -0,0 +1,10 @@
+package com.goormthon.whattoeat.repository;
+
+import com.goormthon.whattoeat.domain.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    List<Ingredient> findByUserId(Integer userId);
+}
