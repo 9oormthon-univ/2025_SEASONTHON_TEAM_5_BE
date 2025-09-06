380a30a1593b4a226ff5c4f849088d41d48709a8
박기찬 <apg0001naver@gmail.com>
2025-09-07 07:17:27 +0900

    [fix] 재료 리스트 API 오류 수정

--- src/main/java/com/goormthon/whattoeat/dto/IngredientListResponse.java
+++ src/main/java/com/goormthon/whattoeat/dto/IngredientListResponse.java
@@ -2,11 +2,15 @@ package com.goormthon.whattoeat.dto;
 
 import com.goormthon.whattoeat.domain.Ingredient;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class IngredientListResponse {
     private Long id;
     private String name;
