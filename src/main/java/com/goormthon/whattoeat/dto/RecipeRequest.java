52423a7714bcc0b518ac7f193888e9738e3e7ffe
박기찬 <apg0001naver@gmail.com>
2025-09-06 22:51:41 +0900

    [chore] gradle.yml에 JWT 관련 환경변수 추가

--- src/main/java/com/goormthon/whattoeat/dto/RecipeRequest.java
+++ src/main/java/com/goormthon/whattoeat/dto/RecipeRequest.java
@@ -9,8 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecipeRequest {
-//    private Integer userId;               // DB에서 재료 조회할 사용자 ID
-                                            // 이후 로그인 구현 시 삭제
     @JsonProperty("cook_time_minutes")
     private Integer cookTimeMinutes;      // 조리 시간(분)
 }
