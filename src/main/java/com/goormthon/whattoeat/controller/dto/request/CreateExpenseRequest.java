2af89e3e799de290cbeee72dad5b3a584e153615
sjmoon00 <paulmoon00@naver.com>
2025-09-06 19:16:46 +0900

    fix: 지출 생성 요청 수정

--- src/main/java/com/goormthon/whattoeat/controller/dto/request/CreateExpenseRequest.java
+++ src/main/java/com/goormthon/whattoeat/controller/dto/request/CreateExpenseRequest.java
@@ -6,7 +6,7 @@ public record CreateExpenseRequest(
         int amount,
         String category,
         LocalDate date,
-        String paymentMethod,
+        String title,
         String memo
 ) {
 }
