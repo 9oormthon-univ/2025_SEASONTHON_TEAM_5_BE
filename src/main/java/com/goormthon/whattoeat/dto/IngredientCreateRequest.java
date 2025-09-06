8be8930ac9e0cecddcdba1b95ac67142099ea781
박기찬 <apg0001naver@gmail.com>
2025-09-06 13:27:27 +0900

    [feat] 제료 등록 dto 추가

--- src/main/java/com/goormthon/whattoeat/dto/IngredientCreateRequest.java
+++ src/main/java/com/goormthon/whattoeat/dto/IngredientCreateRequest.java
@@ -1,4 +1,11 @@
 package com.goormthon.whattoeat.dto;
 
+import java.util.Date;
+
 public class IngredientCreateRequest {
+    private int id;
+    private String name;
+    private int quantity;
+    private String unit;
+    private Date exprirationDate;
 }
