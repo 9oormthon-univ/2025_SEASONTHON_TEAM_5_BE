b8026e9cf8a38d9e1d8775df6beb1c93f40c5c8e
박기찬 <apg0001naver@gmail.com>
2025-09-06 23:37:43 +0900

    [feat] dto 및 service, template 수정

--- src/main/java/com/goormthon/whattoeat/dto/RecipeResponse.java
+++ src/main/java/com/goormthon/whattoeat/dto/RecipeResponse.java
@@ -1,18 +1,7 @@
 package com.goormthon.whattoeat.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class RecipeResponse {
-    private String recipe_name;
-    private List<RecipeIngredientDto> ingredients;
-    private List<String> steps;
-    private String description;
-    private int cook_time_minutes;
+public class RecipeListResponse {
+    List<RecipeResponse> recipeResponseList;
 }
